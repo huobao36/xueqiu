fec9ebfc10ce023397e0ecb586d7d795afee4792
Louis Deng <denglu@snowballfinance.com>
2012-04-24 08:41:06 +0800

    modified:   src/main/java/com/snowballfinance/kddc/job/ItemSimilarityJob.java

--- src/main/java/com/snowballfinance/kddc/job/ItemSimilarityJob.java
+++ src/main/java/com/snowballfinance/kddc/job/ItemSimilarityJob.java
@@ -5,25 +5,69 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 
 import javax.lang.model.SourceVersion;
 
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.apache.log4j.Logger;
 
 public class ItemSimilarityJob extends Configured implements Tool{
 
+	private final static List<Long> itemIds = new LinkedList<Long>();
+	private static Logger logger = Logger.getLogger(ItemSimilarityJob.class);
+	
+	public static void main(String[] args) 
+	{
+		if(args.length < 2)
+			System.err.println("Usage: ItemSimilarityJob /path/to/input /path/to/output");
+		int res = 0;
+		try {
+			res = ToolRunner.run(new Configuration(), new ItemSimilarityJob(), args);
+		} catch (Exception e) {
+			logger.error("JaccardIndexJob Tool Runner Err.", e);
+		}
+		System.exit(res);
+	}
+	
 	@Override
 	public int run(String[] arg0) throws Exception {
+		JobConf conf = new JobConf(ItemSimilarityJob.class);
+		conf.setJobName("Item Simarity Job");
+		conf.setOutputKeyClass(Text.class);
+		conf.setOutputValueClass(FloatWritable.class);
+		Job job = new Job(conf);
+		job.setMapperClass(CateAndKeyWordMapper.class);
+		job.setReducerClass(ScoreReducer.class);
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+		FileInputFormat.addInputPaths(job, arg0[0]);
+		FileOutputFormat.setOutputPath(job, new Path(arg0[1]));
+		job.waitForCompletion(true);
 		return 0;
 	}
-
-	private float getCategorySimScore(String srcCat, String destCat) {
+	
+	private static final int FULLSCORE = 8 + 4 + 2 + 2; 
+	
+	private static float getCategorySimScore(String srcCat, String destCat) {
 		String[] srcCats = srcCat.split(".");
 		String[] destCats = destCat.split(".");
 		float res = 0f;
@@ -39,7 +83,7 @@ public class ItemSimilarityJob extends Configured implements Tool{
 	}
 	
 	
-	private float getKeyWordSimScore(String srcKeyWord, String destKeyWord) {
+	private static float getKeyWordSimScore(String srcKeyWord, String destKeyWord) {
 		String[] srcKeyWords = srcKeyWord.split(";");
 		String[] destKeyWords = destKeyWord.split(";");
 		Set<String> srcKeyWordSet = new HashSet<String>(Arrays.asList(srcKeyWords));
@@ -50,8 +94,40 @@ public class ItemSimilarityJob extends Configured implements Tool{
 		return totalcount == 0 ? 0 : commoncount / (totalcount - commoncount);
 	}
 	
-	public static class CateAndKeyWordMapper extends Mapper<LongWritable, Text, Text, StrFloatPair>
+
+	private static class Item
 	{
+		public String getKey() {
+			return key;
+		}
+		public void setKey(String key) {
+			this.key = key;
+		}
+		public String getCategory() {
+			return category;
+		}
+		public void setCategory(String cat) {
+			this.category = cat;
+		}
+		public String getKeyWords() {
+			return keywords;
+		}
+		public void setKeyWords(String keywords) {
+			this.keywords = keywords;
+		}
+		String key;
+		String category;
+		String keywords;
+	};
+	
+	private static List<Item> itemKeyValPairList = new LinkedList<Item>();
+	
+	public static class CateAndKeyWordMapper extends Mapper<LongWritable, Text, Text, FloatWritable>
+	{
+		private static float computeSocre(String srccat, String srckeywords, Item destItem )
+		{
+			return (getCategorySimScore(srccat, destItem.getCategory()) + getKeyWordSimScore(srckeywords, destItem.getKeyWords())) / 16;
+		}
 		
 		@Override 
 		protected void map(LongWritable ikey, Text ival,
@@ -59,9 +135,28 @@ public class ItemSimilarityJob extends Configured implements Tool{
 				throws IOException, InterruptedException {
 			String[] keyValues = ival.toString().split(" ");
 			String key = keyValues[0];
-			
+			String cat = keyValues[1];
+			String keywords = keyValues[2];
+			for(Item item : itemKeyValPairList)
+			{
+				float score = computeSocre(cat, keywords, item);
+				context.write(new Text(key + " " + item.getKey()), new FloatWritable(score));
+			}
 		}
 	}
 	
+	public static class ScoreReducer extends Reducer<Text, FloatWritable, Text, FloatWritable> 
+	{
+		@Override
+		protected void reduce(Text key, Iterable<FloatWritable> scores, Context ctx)
+			throws IOException, InterruptedException 
+		{
+			Iterator<FloatWritable> iter = scores.iterator();
+			float score = iter.next().get();
+			ctx.write(key, new FloatWritable(score));
+			String[] keyPairs = key.toString().split(" ");
+			ctx.write(new Text(keyPairs[1] + " " + keyPairs[0]), new FloatWritable(score));
+		}
+	}
 	
 }

226bddb37b1e6d78a1a99c1f84f44a0dc36c2551
Louis Deng <denglu36@yahoo.com.cn>
2012-05-10 11:47:55 +0800

    new file:   shell/FilterItemJob.sh

---
+++ shell/FilterItemJob.sh
@@ -0,0 +1,13 @@
+KDDC="/data/deploy/recommender/shell/lib/kddc-1.0.0.jar"
+path="`dirname $0`"
+INPUT_PATH=$1
+OUTPUT_PATH=$2
+if [ ! -n "$INPUT_PATH" -o ! -n "$OUTPUT_PATH" ];then
+    echo 'Usage: ./FilterItemJob INPUT_PATH OUTPUT_PATH  '
+    exit 0
+else
+    echo "INPUT_PATH: $INPUT_PATH, OUTPUT_PATH:$OUTPUT_PATH"
+fi
+
+
+$path/Job.sh com.snowballfinance.kddc.job.FilterItemJob "$1" "$2" 

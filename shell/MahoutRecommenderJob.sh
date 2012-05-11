f2a360635cddf7b83f6352abe5d8e2fdd9309004
Louis Deng <denglu@xueqiu.com>
2012-05-11 12:05:33 +0800

    modified:   shell/MahoutRecommenderJob.sh

--- shell/MahoutRecommenderJob.sh
+++ shell/MahoutRecommenderJob.sh
@@ -9,7 +9,7 @@ else
     echo "INPUT_PATH: $INPUT_PATH, OUTPUT_PATH:$OUTPUT_PATH"
 fi
 
-$path/Job.sh com.snowballfinance.kddc.job.MahoutRecommenderJob --input "$1" --output "$2" --usersFile "$3" --itemsFile "$4" --maxPrefsPerUser "$5" 
+$path/Job.sh com.snowballfinance.kddc.job.MahoutRecommenderJob --input "$1" --output "$2" --usersFile "$3" --itemsFile "$4" --maxPrefsPerUser "$5"  --similarityClassname SIMILARITY_COSINE
 
 
 
